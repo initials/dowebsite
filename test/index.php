cfb975bde5eb95d0c3f8b8d2b3b88c956afd276c
Shane Brouwer <shane@findesign.com.au>
2013-12-18 12:55:39 +1100

    x

--- test/index.php
+++ test/index.php
@@ -13,12 +13,51 @@ foreach( $xml->children() as $child )
 		case("name"):
 			echo "<b>".$child."</b> <br>";
 			break;		
-		case("download"):
-			echo '<a href="Download this game"'.$child.">Download</a><br>";
+		case("sourcecode"):
+			echo "<b> Source code available: ".$child."</b> <br>";
 			break;	
+		case("download"):
+			foreach( $child->children() as $dl ) {
+				echo 'Download this game for: '.$dl->getName(). ' link: '.$dl.'<br>';
+			}
+			break;	
+		case("credits"):
+			foreach( $child->children() as $dl ) {
+				echo 'Credits: '.$dl->getName(). ' link: '.$dl.'<br>';
+			}
+			break;	
+		case("links"):
+			echo '<span id="text-place">';
+			foreach( $child->children() as $dl ) {
+				echo 'Links: '.$dl->getName(). ' link: '.$dl.'<br>';
+			}
+			echo '</span>';
+			break;							
 		case("image"):
 			echo '<img src="'.$child.'" alt="InitialsLogo Video Games Retro Video Games 8-bit"> <br>';
 			break;	
+
+		case("screenshots"):
+			if ($handle = opendir('./screenshots')) {
+				while (false !== ($entry = readdir($handle))) {
+					if ($entry != "." && $entry != ".." && substr($entry,0,1) != "_" && substr($entry,-4) != ".log" && substr($entry,0,6) != "images" && substr($entry,0,8) != "trailers" && substr($entry,0,9) != "error_log") {
+						echo '<img src="screenshots/'.$entry.'" /><br>';
+						echo '<br>';
+
+					}
+					
+				}
+
+			}
+			closedir($handle);
+			break;	
+
+
+
+
+
+
+
 	}
 	echo "<br>";
 }
